4e0f8e6e3653e33bbffdc5c01fc183711c94672e
josesouzaa <josee.souzaaa@gmail.com>
2022-07-25 21:13:20 -0300

    Propriedades no post

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,51 @@ import { Header } from './components/Header'
 import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/josesouzaa.png',
+      name: 'José de Souza',
+      role: 'Web developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-07-24 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/josesouzaa.png',
+      name: 'José de Souza',
+      role: 'Web developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-07-23 20:00:00')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -11,14 +56,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="José"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia vitae autem dolorum maiores, rem perspiciatis velit. Sed velit asperiores maiores consequuntur, culpa, facere impedit unde nemo praesentium pariatur, autem a!"
-          />
-          <Post
-            author="Diego"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia vitae autem dolorum maiores, rem perspiciatis velit. Sed velit asperiores maiores consequuntur, culpa, facere impedit unde nemo praesentium pariatur, autem a!"
-          />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,63 +1,74 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 interface PostProps {
-  author: string
-  content: string
+  id: number
+  author: {
+    avatarUrl: string
+    name: string
+    role: string
+  }
+  content: {
+    type: string
+    content: string
+  }[]
+  publishedAt: Date
 }
 
-export function Post({ author, content }: PostProps) {
+export function Post({ id, author, content, publishedAt }: PostProps) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className="bg-brand-gray-800 rounded-lg p-10 first-of-type:mt-0 mt-8">
       <header className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <Avatar hasBorder />
+          <Avatar hasBorder src={author.avatarUrl} />
 
           <div className="flex flex-col">
             <strong className="text-brand-gray-100 leading-[1.6]">
-              {author}
+              {author.name}
             </strong>
             <span className="text-brand-gray-400 text-sm leading-[1.6]">
-              Web developer
+              {author.role}
             </span>
           </div>
         </div>
 
         <time
           className="text-sm text-brand-gray-400"
-          title="11 de Maio as 08:13"
-          dateTime="2022-05-11 00:13:30"
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
         >
-          Publicado há 1 hora
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
-      <div className="leading-[1.6] text-brand-gray-300 mt-6">
-        <p className="mt-4">Fala galeraa 👋</p>
+      <div className="leading-[1.6] text-brand-gray-300 mt-6 post-content">
+        {content.map((line) => {
+          switch (line.type) {
+            case 'paragraph':
+              return <p>{line.content}</p>
 
-        <p className="mt-4">
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-
-        <p className="mt-4">
-          👉{' '}
-          <a
-            className="font-bold text-brand-green-500 hover:text-brand-green-300"
-            href="#"
-          >
-            jane.design/doctorcare
-          </a>
-        </p>
-
-        <p className="mt-4">
-          <a
-            className="font-bold text-brand-green-500 hover:text-brand-green-300"
-            href="#"
-          >
-            #novoprojeto #nlw #rocketseat
-          </a>
-        </p>
+            case 'link':
+              return (
+                <p>
+                  <a href="#">{line.content}</a>
+                </p>
+              )
+          }
+        })}
       </div>
 
       <form className="w-full mt-6 pt-6 border-t border-brand-gray-600 group">
